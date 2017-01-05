346ebf44e6836134c1f09c256270fb23301f6a1f
YaChen Yan <yanyachen@users.noreply.github.com>
2017-01-05 23:00:54 +0800

    Fix Integer Overflow Bug
    Fix Integer Overflow Bug

--- R/Classification.R
+++ R/Classification.R
@@ -334,8 +334,8 @@ MultiLogLoss <- function(y_pred, y_true) {
 
 AUC <- function(y_pred, y_true) {
   rank <- rank(y_pred)
-  n_pos <- sum(y_true == 1)
-  n_neg <- sum(y_true == 0)
+  n_pos <- as.double(sum(y_true == 1))
+  n_neg <- as.double(sum(y_true == 0))
   AUC <- (sum(rank[y_true == 1]) - n_pos * (n_pos + 1) / 2) / (n_pos * n_neg)
   return(AUC)
 }

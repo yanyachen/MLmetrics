83a403cc86da496c057f987847a3a05235c22b1a
YaChen Yan <yanyachen@users.noreply.github.com>
2017-01-21 10:17:28 +0800

    Update KS_Stat Function
    Update KS_Stat Function

--- R/Classification.R
+++ R/Classification.R
@@ -446,16 +446,9 @@ GainAUC <- function(y_pred, y_true) {
 #' @export
 
 KS_Stat <- function(y_pred, y_true) {
-  n_pos <- sum(y_true == 0)
-  n_neg <- sum(y_true == 1)
-  rpp_vec <- numeric(length = 0)
-  rnp_vec <- numeric(length = 0)
-  for (i in sort(y_pred)) {
-    pos_neg <- y_true[y_pred <= i]
-    rpp_vec <- append(rpp_vec, sum(pos_neg == 0) / n_pos)
-    rnp_vec <- append(rnp_vec, sum(pos_neg == 1) / n_neg)
-  }
-  KS_Stat <- max(rpp_vec - rnp_vec) * 100
+  pred_obj <- ROCR::prediction(y_pred, y_true)
+  perf_obj <- ROCR::performance(pred_obj, measure = "tpr", x.measure = "fpr")
+  KS_Stat <- max(perf_obj@y.values[[1]] - perf_obj@x.values[[1]])
   return(KS_Stat)
 }
 

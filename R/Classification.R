76807aca8f0b06c731eb821f999af1b5268f24d9
Renato Panda <panda@dei.uc.pt>
2017-03-04 19:30:20 +0000

    added multi-class classification metrics: micro/macro F1 Score, Precision and Recall.

--- R/Classification.R
+++ R/Classification.R
@@ -119,6 +119,101 @@ Precision <- function(y_true, y_pred, positive = NULL) {
 }
 
 
+#' @title Precision (micro averaged)
+#'
+#' @description
+#' Compute the precision score of multi-class problem using the "micro" average.
+#' details: https://sebastianraschka.com/faq/docs/multiclass-metric.html
+#' 
+#' @param y_pred Predicted labels vector, as returned by a classifier
+#' @param y_true Ground truth (correct) labels vector
+#' @param labels An optional vector containing the list of the existent
+#'   (unique) labels.
+#' @return Precision (micro averaged)
+#' @examples
+#' labels <- c("Q1","Q2","Q3","Q4")
+#' truth <- sample(labels, 10, replace = TRUE)
+#' pred <- sample(labels, 10, replace = TRUE)
+#' Precision_micro(y_pred = pred, y_true = truth, labels)
+#' @export
+
+Precision_micro <- function(y_true, y_pred, labels = NULL) {
+  Confusion_DF <- ConfusionDF(y_pred, y_true)
+  
+  if (is.null(labels) == TRUE) labels <- unique(c(y_true, y_pred))
+  # this is not bulletproof since there might be labels missing (in strange cases)
+  # in strange cases where they existed in training set but are missing from test ground truth and predictions.
+  
+  TP <- c()
+  FP <- c()
+  for (i in c(1:length(labels))) {
+    positive <- labels[i]
+    
+    # it may happen that a label is never predicted (missing from y_pred) but exists in y_true
+    # in this case ConfusionDF will not have these lines and thus the simplified code crashes
+    # TP[i] <- as.integer(subset(Confusion_DF, y_true==positive & y_pred==positive)["Freq"])
+    # FP[i] <- as.integer(sum(subset(Confusion_DF, y_true!=positive & y_pred==positive)["Freq"]))
+    
+    # workaround:
+    # i don't want to change ConfusionDF since i don't know if the current behaviour is a feature or a bug.
+    tmp <- subset(Confusion_DF, y_true==positive & y_pred==positive)["Freq"]
+    TP[i] <- if (nrow(tmp)==0) 0 else as.integer(tmp)
+    
+    tmp <- subset(Confusion_DF, y_true!=positive & y_pred==positive)["Freq"]
+    FP[i] <- if (nrow(tmp)==0) 0 else as.integer(sum(tmp))
+  }
+  Precision_micro <- sum(TP) / (sum(TP) + sum(FP))
+  return(Precision_micro)
+}
+
+
+#' @title Precision (macro averaged)
+#'
+#' @description
+#' Compute the precision score of multi-class problem using the "macro" average.
+#' details: https://sebastianraschka.com/faq/docs/multiclass-metric.html
+#' 
+#' @param y_pred Predicted labels vector, as returned by a classifier
+#' @param y_true Ground truth (correct) labels vector
+#' @param labels An optional vector containing the list of the existent
+#'   (unique) labels.
+#' @return Precision (macro averaged)
+#' @examples
+#' labels <- c("Q1","Q2","Q3","Q4")
+#' truth <- sample(labels, 10, replace = TRUE)
+#' pred <- sample(labels, 10, replace = TRUE)
+#' Precision_macro(y_pred = pred, y_true = truth, labels)
+#' @export
+
+Precision_macro <- function(y_true, y_pred, labels = NULL) {
+  Confusion_DF <- ConfusionDF(y_pred, y_true)
+  
+  if (is.null(labels) == TRUE) labels <- unique(c(y_true, y_pred))
+  # this is not bulletproof since there might be labels missing (in strange cases)
+  # in strange cases where they existed in training set but are missing from test ground truth and predictions.
+  
+  Prec <- c()
+  for (i in c(1:length(labels))) {
+    positive <- labels[i]
+    
+    # it may happen that a label is never predicted (missing from y_pred) but exists in y_true
+    # in this case ConfusionDF will not have these lines and thus the simplified code crashes
+    # Prec[i] <- Precision(y_true, y_pred, positive = labels[i])
+    
+    # workaround:
+    tmp <- subset(Confusion_DF, y_true==positive & y_pred==positive)["Freq"]
+    TP <- if (nrow(tmp)==0) 0 else as.integer(tmp)
+    tmp <- subset(Confusion_DF, y_true!=positive & y_pred==positive)["Freq"]
+    FP <- if (nrow(tmp)==0) 0 else as.integer(sum(tmp))
+    
+    Prec[i] <- TP/(TP+FP)
+  }
+  Prec[is.na(Prec)] <- 0
+  Precision_macro <- mean(Prec) # sum(Prec) / length(labels)
+  return(Precision_macro)
+}
+
+
 #' @title Recall
 #'
 #' @description
@@ -145,6 +240,101 @@ Recall <- function(y_true, y_pred, positive = NULL) {
   FN <- as.integer(sum(subset(Confusion_DF, y_true==positive & y_pred!=positive)["Freq"]))
   Recall <- TP/(TP+FN)
   return(Recall)
+}
+
+
+#' @title Recall (micro averaged)
+#'
+#' @description
+#' Compute the recall score of multi-class problem using the "micro" average.
+#' details: https://sebastianraschka.com/faq/docs/multiclass-metric.html
+#'
+#' @param y_pred Predicted labels vector, as returned by a classifier
+#' @param y_true Ground truth (correct) labels vector
+#' @param labels An optional vector containing the list of the existent
+#'   (unique) labels.
+#' @return Recall (micro averaged)
+#' @examples
+#' labels <- c("Q1","Q2","Q3","Q4")
+#' truth <- sample(labels, 10, replace = TRUE)
+#' pred <- sample(labels, 10, replace = TRUE)
+#' Recall_micro(y_pred = pred, y_true = truth, labels)
+#' @export
+
+Recall_micro <- function(y_true, y_pred, labels = NULL) {
+  Confusion_DF <- ConfusionDF(y_pred, y_true)
+  
+  if (is.null(labels) == TRUE) labels <- unique(c(y_true, y_pred))
+  # this is not bulletproof since there might be labels missing (in strange cases)
+  # in strange cases where they existed in training set but are missing from test ground truth and predictions.
+  
+  TP <- c()
+  FN <- c()
+  for (i in c(1:length(labels))) {
+    positive <- labels[i]
+    
+    # short version, comment out due to bug or feature of Confusion_DF
+    # TP[i] <- as.integer(subset(Confusion_DF, y_true==positive & y_pred==positive)["Freq"])
+    # FP[i] <- as.integer(sum(subset(Confusion_DF, y_true==positive & y_pred!=positive)["Freq"]))
+    
+    # workaround:
+    tmp <- subset(Confusion_DF, y_true==positive & y_pred==positive)["Freq"]
+    TP[i] <- if (nrow(tmp)==0) 0 else as.integer(tmp)
+
+    tmp <- subset(Confusion_DF, y_true==positive & y_pred!=positive)["Freq"]
+    FN[i] <- if (nrow(tmp)==0) 0 else as.integer(sum(tmp))
+  }
+  Recall_micro <- sum(TP) / (sum(TP) + sum(FN))
+  return(Recall_micro)
+}
+
+
+#' @title Recall (macro averaged)
+#'
+#' @description
+#' Compute the recall score of multi-class problem using the "macro" average.
+#' details: https://sebastianraschka.com/faq/docs/multiclass-metric.html
+#'
+#' @param y_pred Predicted labels vector, as returned by a classifier
+#' @param y_true Ground truth (correct) labels vector
+#' @param labels An optional vector containing the list of the existent
+#'   (unique) labels.
+#' @return Recall (macro averaged)
+#' @examples
+#' labels <- c("Q1","Q2","Q3","Q4")
+#' truth <- sample(labels, 10, replace = TRUE)
+#' pred <- sample(labels, 10, replace = TRUE)
+#' Recall_macro(y_pred = pred, y_true = truth, labels)
+#' @export
+
+Recall_macro <- function(y_true, y_pred, labels = NULL) {
+  Confusion_DF <- ConfusionDF(y_pred, y_true)
+  
+  if (is.null(labels) == TRUE) labels <- unique(c(y_true, y_pred))
+  # this is not bulletproof since there might be labels missing (in strange cases)
+  # in strange cases where they existed in training set but are missing from test ground truth and predictions.
+  
+  Rec <- c()
+  for (i in c(1:length(labels))) {
+    positive <- labels[i]
+    
+    # short version, comment out due to bug or feature of Confusion_DF
+    # TP[i] <- as.integer(subset(Confusion_DF, y_true==positive & y_pred==positive)["Freq"])
+    # FP[i] <- as.integer(sum(subset(Confusion_DF, y_true==positive & y_pred!=positive)["Freq"]))
+    
+    # workaround:
+    tmp <- subset(Confusion_DF, y_true==positive & y_pred==positive)["Freq"]
+    TP <- if (nrow(tmp)==0) 0 else as.integer(tmp)
+    
+    tmp <- subset(Confusion_DF, y_true==positive & y_pred!=positive)["Freq"]
+    FN <- if (nrow(tmp)==0) 0 else as.integer(sum(tmp))
+    
+    Rec[i] <- TP/(TP+FN)
+  }
+
+  Rec[is.na(Rec)] <- 0
+  Recall_macro <- mean(Rec) # sum(Rec) / length(labels)
+  return(Recall_macro)
 }
 
 
@@ -232,6 +422,60 @@ F1_Score <- function(y_true, y_pred, positive = NULL) {
   Recall <- Recall(y_true, y_pred, positive)
   F1_Score <- 2 * (Precision * Recall) / (Precision + Recall)
   return(F1_Score)
+}
+
+
+#' @title F1 Score (micro averaged)
+#'
+#' @description
+#' Compute the F1 Score of multi-class problem using the "micro" average.
+#' details: https://sebastianraschka.com/faq/docs/multiclass-metric.html
+#'
+#' @param y_pred Predicted labels vector, as returned by a classifier
+#' @param y_true Ground truth (correct) labels vector
+#' @param labels An optional vector containing the list of the existent
+#'   (unique) labels.
+#' @return F1 Score (micro averaged)
+#' @examples
+#' labels <- c("Q1","Q2","Q3","Q4")
+#' truth <- sample(labels, 10, replace = TRUE)
+#' pred <- sample(labels, 10, replace = TRUE)
+#' F1_Score_micro(y_pred = pred, y_true = truth, labels)
+#' @export
+
+F1_Score_micro <- function(y_true, y_pred, labels = NULL) {
+  if (is.null(labels) == TRUE) labels <- unique(c(y_true, y_pred)) # possible problems if labels are missing from y_*
+  Precision <- Precision_micro(y_true, y_pred, labels)
+  Recall <- Recall_micro(y_true, y_pred, labels)
+  F1_Score_micro <- 2 * (Precision * Recall) / (Precision + Recall)
+  return(F1_Score_micro)
+}
+
+
+#' @title F1 Score (macro averaged)
+#'
+#' @description
+#' Compute the F1 Score of multi-class problem using the "macro" average.
+#' details: https://sebastianraschka.com/faq/docs/multiclass-metric.html
+#'
+#' @param y_pred Predicted labels vector, as returned by a classifier
+#' @param y_true Ground truth (correct) labels vector
+#' @param labels An optional vector containing the list of the existent
+#'   (unique) labels.
+#' @return F1 Score (macro averaged)
+#' @examples
+#' labels <- c("Q1","Q2","Q3","Q4")
+#' truth <- sample(labels, 10, replace = TRUE)
+#' pred <- sample(labels, 10, replace = TRUE)
+#' F1_Score_macro(y_pred = pred, y_true = truth, labels)
+#' @export
+
+F1_Score_macro <- function(y_true, y_pred, labels = NULL) {
+  if (is.null(labels) == TRUE) labels <- unique(c(y_true, y_pred)) # possible problems if labels are missing from y_*
+  Precision <- Precision_macro(y_true, y_pred, labels)
+  Recall <- Recall_macro(y_true, y_pred, labels)
+  F1_Score_macro <- 2 * (Precision * Recall) / (Precision + Recall)
+  return(F1_Score_macro)
 }
 
 
